e6e1a4623345474e7fa7390219874457c802bd0f
Paul Lee <contact@paullee.dev>
2024-02-20 01:13:19 -0500

    updated resume url to point to 2024-02-16 version

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,5 +1,5 @@
 const urls = {
-  resume: "https://cdn.paullee.dev/files/resume/20240121.pdf",
+  resume: "https://cdn.paullee.dev/files/resume/20240216.pdf",
   email: "contact@paullee.dev",
   github: "https://github.com/HillcrestEnigma",
   linkedin: "https://www.linkedin.com/in/paul-lee-dev/",
